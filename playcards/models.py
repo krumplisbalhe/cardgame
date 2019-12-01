85950a7f8d7a3fc2a9e6d36846ef01563d635b01
rita.radovics <krumplisbalhe@gmail.com>
2019-12-01 17:22:51 +0100

    before API

--- playcards/models.py
+++ playcards/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_delete
 
 class Card(models.Model):
   packId = models.PositiveIntegerField(default=1)
@@ -8,3 +9,8 @@ class Card(models.Model):
 
   def __str__(self):
     return self.cardText
+
+def after_delete_post(sender, instance, **kwargs):
+  print("******Someone deleted a card")
+
+post_delete.connect(after_delete_post, sender=Card)
